b081a58f6540e3705a51f8795494fe4d45a93da3
Elena <57794968+3le81@users.noreply.github.com>
2024-01-10 23:05:15 +0000

    fixed issue1 in file details

--- T01 - Your First Computer Program and Using Variables/details.py
+++ T01 - Your First Computer Program and Using Variables/details.py
@@ -7,7 +7,8 @@
 """
 
 name = input("Enter your name: ")
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 house_number = input("Enter your house number: ")
 street_name = input("Enter your street name: ")
-print(f"This is {name}, is {age} years old and lives at {house_number} on {street_name}.")
+print(
+    f"This is {name}, is {age} years old and lives at {house_number} on {street_name}.")
